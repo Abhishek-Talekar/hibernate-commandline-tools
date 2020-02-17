566715be79e0d73a68130fb3c6264c3a5b545e05
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-17 23:13:35 +0530

    Second commit with hbm.xml & cfg.xml

--- HibernateCommandLine/src/com/cl/entitties/StoreInventory.java
+++ HibernateCommandLine/src/com/cl/entitties/StoreInventory.java
@@ -1,5 +1,55 @@
 package com.cl.entitties;
 
-public class StoreInventory {
+import java.io.Serializable;
+
+public class StoreInventory implements Serializable{
 	protected int storeId;
+	protected String storeName;
+	protected String storeAddressLine1;
+	protected String storeCity;
+	protected String storeState;
+	protected int zip;
+	public int getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreAddressLine1() {
+		return storeAddressLine1;
+	}
+	public void setStoreAddressLine1(String storeAddressLine1) {
+		this.storeAddressLine1 = storeAddressLine1;
+	}
+	public String getStoreCity() {
+		return storeCity;
+	}
+	public void setStoreCity(String storeCity) {
+		this.storeCity = storeCity;
+	}
+	public String getStoreState() {
+		return storeState;
+	}
+	public void setStoreState(String storeState) {
+		this.storeState = storeState;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	@Override
+	public String toString() {
+		return "StoreInventory [storeId=" + storeId + ", storeName=" + storeName + ", storeAddressLine1="
+				+ storeAddressLine1 + ", storeCity=" + storeCity + ", storeState=" + storeState + ", zip=" + zip + "]";
+	}
+	
+	
 }
